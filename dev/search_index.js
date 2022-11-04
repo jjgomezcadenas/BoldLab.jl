377bc609fa7c1dde4807ba7fc13215632346b348
Documenter.jl <documenter@juliadocs.github.io>
2022-11-04 10:10:33 +0000

    build based on 214bd5a

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = BoldLab","category":"page"},{"location":"#BoldLab","page":"Home","title":"BoldLab","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for BoldLab.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [BoldLab]","category":"page"}]
+}
