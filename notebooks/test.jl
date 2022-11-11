f0aead3a8a7182abf92ea80e98cd4e55485869c4
jjgomezcadenas <jjgomezcadenas@gmail.com>
2022-11-11 17:14:22 +0100

    First commit of test notebook

---
+++ notebooks/test.jl
@@ -0,0 +1,29 @@
+### A Pluto.jl notebook ###
+# v0.19.14
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 54d5563a-61db-11ed-2e64-a776bc2788f4
+md"This is a test"
+
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+julia_version = "1.8.1"
+manifest_format = "2.0"
+project_hash = "da39a3ee5e6b4b0d3255bfef95601890afd80709"
+
+[deps]
+"""
+
+# ╔═╡ Cell order:
+# ╠═54d5563a-61db-11ed-2e64-a776bc2788f4
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
